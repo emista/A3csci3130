3e18bed3506dd0b5a81b88dfd00df6368b12ef14
Yihong Yao <yaoyihongnw@126.com>
2018-07-08 15:46:55 -0300

    add tests

---
+++ app/src/androidTest/java/com/acme/a3csci3130/CreateContactTest.java
@@ -0,0 +1,60 @@
+package com.acme.a3csci3130;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class CreateContactTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule=new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void createContact(){
+        //check submitButton exist
+        onView(withId(R.id.submitButton)).perform(click());
+
+        //check edit text name
+        onView(withId(R.id.name)).perform(typeText("Alicia"));
+        closeSoftKeyboard();
+        onView(withId(R.id.name)).check(matches(withText("Alicia")));
+
+        //check edit text number
+        onView(withId(R.id.number)).perform(typeText("012345678"));
+        closeSoftKeyboard();
+        onView(withId(R.id.number)).check(matches((withText("012345678"))));
+
+        //check edit text primary
+        onView(withId(R.id.primary)).perform(typeText("Fisher"));
+        closeSoftKeyboard();
+        onView(withId(R.id.primary)).check(matches(withText("Fisher")));
+
+        //check edit text province
+        onView(withId(R.id.province)).perform(typeText("NS"));
+        closeSoftKeyboard();
+        onView(withId(R.id.province)).check(matches(withText("NS")));
+
+        //check edit text address
+        onView(withId(R.id.address)).perform(typeText("this is a test address"));
+        closeSoftKeyboard();
+        onView(withId(R.id.address)).check(matches(withText("this is a test address")));
+
+        onView(withId(R.id.submitButton)).perform(click());
+
+    }
+
+}
---
+++ app/src/androidTest/java/com/acme/a3csci3130/UpdateContact.java
@@ -0,0 +1,26 @@
+package com.acme.a3csci3130;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class UpdateContact {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule=new ActivityTestRule<>(MainActivity.class);
+    
+}
