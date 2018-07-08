82de7262595804061daff0a30136f748e99d0093
Yihong Yao <yaoyihongnw@126.com>
2018-07-08 16:18:48 -0300

    update contact test

--- app/src/androidTest/java/com/acme/a3csci3130/ContactTest.java
+++ app/src/androidTest/java/com/acme/a3csci3130/ContactTest.java
@@ -9,17 +9,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class CreateContactTest {
+public class ContactTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule=new ActivityTestRule<>(MainActivity.class);
 
@@ -55,6 +58,23 @@ public class CreateContactTest {
 
         onView(withId(R.id.submitButton)).perform(click());
 
+    }
+
+    @Test
+    public void updateContact(){
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
+        onView(withId(R.id.name)).perform(clearText());
+        onView(withId(R.id.name)).perform(typeText("Beta"));
+        closeSoftKeyboard();
+        onView(withId(R.id.updateButton)).perform(click());
+
+    }
+
+    @Test
+    public void deleteContact() throws InterruptedException {
+        Thread.sleep(5000);
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
+        onView(withId(R.id.deleteButton)).perform(click());
     }
 
 }
